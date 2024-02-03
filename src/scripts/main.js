dd4e383ac6edf931540ec8a695c0cd4760176469
pedroDev <playstarpedro@gmail.com>
2024-02-03 17:27:59 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
+        evento.preventDefault();
+
+        let numeroMaximo = parseInt(document.getElementById('numero-maximo').value)
+        
+        let numeroAleatorio = Math.random() * numeroMaximo;
+        numeroAleatorio = Math.floor(numeroAleatorio + 1)
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio
+
+        document.querySelector('.resultado').style.display = 'block';
+    })
+})
